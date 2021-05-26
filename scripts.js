9ada156686de358f4c2e775b607c4d1a9c2051c4
Holly Henske <hcover333@gmail.com>
2021-05-26 16:50:41 -0500

    the first bit of code

--- scripts.js
+++ scripts.js
@@ -1,2 +1,11 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function() {
+    console.log('loaded: 100%');
+});
+
+function liftOff(){
+    window.confirm("Confirm that the shuttle is ready for takeoff.")
+    }
+    let buttonT = document.getElementById("liftoffButton");
+    buttonT.addEventListener("click", liftOff);
