ed384da83df5d3d0bf6accbec362e3d833e1e934
Holly Henske <hcover333@gmail.com>
2021-05-26 20:07:29 -0500

    added to js file

--- scripts.js
+++ scripts.js
@@ -1,11 +1,37 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
-window.addEventListener("load", function() {
-    console.log('loaded: 100%');
-});
+ 
 
-function liftOff(){
-    window.confirm("Confirm that the shuttle is ready for takeoff.")
-    }
-    let buttonT = document.getElementById("liftoffButton");
-    buttonT.addEventListener("click", liftOff);
+function init() {
+    const shuttleBackground = document.getElementById("shuttleBackground");
+    const shuttleHeight = document.getElementById('spaceShuttleHeight');
+    const flightStatus = document.getElementById('flightStatus');
+    const takeOffButton = document.getElementById('takeOff');
+    const landingButton = document.getElementById('landing');
+
+
+    //init loads default settings
+    //everything that says id, we need to initialize
+   function confirmTakeOff() {
+       let response = window.confirm("Is the shuttle ready?");
+       if (response) {
+           flightStatus.innerHTML = "Shuttle in flight."
+           shuttleBackground.style.backgroundColor = "blue"
+           shuttleHeight.innerHTML = '10000'
+       }
+   }
+   takeOffButton.addEventListener("click",confirmTakeOff);
+
+   function landingConfirm() {
+       let alert = window.confirm("The shuttle is landing. Landing gear engaged.")
+       shuttleBackground.style.backgroundColor = ""
+       shuttleHeight.innerHTML = "0"
+   }
+}
+    landingButton.addEventListener("click", landingConfirm);
+
+
+}
+
+
+    window.addEventListener("load", init);
